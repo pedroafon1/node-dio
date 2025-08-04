0f00a5d344df317ab21c9505d01a0dc6421f437e
Pedro Afonso <83741439+pedroafon1@users.noreply.github.com>
2025-08-04 15:55:02 -0300

    start

--- 02-MARIO KART JS/src/index.js
+++ 02-MARIO KART JS/src/index.js
@@ -6,10 +6,22 @@ const player1 = {
   points: 0, 
 }
 
-const player 2 = {
+const player2 = {
   name : "Luigi",
   speed : 3,
   maneuverability: 4,
   power: 4,
   points: 0
 }
+
+async function rollDice() {
+  Math.floor(Math.random() * 6) + 1;
+}
+
+(async function main() {
+  console.log(
+    `Corrida entre ${player1.name} e ${player2.name} começando...\n`,
+  );
+  
+})();
+
